6dea01a8fc1706875fcf587eb266f3b1583a1d44
sgrzegorz <grzegorzsroka2@gmail.com>
2018-06-01 21:30:35 +0200

    fghj

--- cw7/zad1/shared_memory.c
+++ cw7/zad1/shared_memory.c
@@ -1,29 +1,29 @@
 #include "shared_memory.h"
 
-int getSize(Fifo * fifo){
-    return fifo ->nelements;
-}
 
 int push(Fifo *fifo,int val){
-    if(getSize(fifo) == QUEUE_SIZE) return 0;
+    if(fifo ->nelements == QUEUE_SIZE) return -1; // It'll be error of implementation
+    if(fifo ->nelements == fifo->size) return 0; //no place to push 
     fifo -> tail = (fifo->tail +1) % fifo->size;
-    fifo ->nelements++;
     fifo->queue[fifo->tail] = val;
+    fifo ->nelements++;
 }
 
 int pop(Fifo * fifo){
-    if(getSize(fifo) == 0) return 0;
-    int val = fifo->queue[fifo->head];
+    if(fifo ->nelements == 0) return 0; // no element to pop
     fifo->head = (fifo->head +1) % fifo->size;
+    int val = fifo->queue[fifo->head];
+    fifo->queue[fifo->head] = -1;
     fifo-> nelements--;
     return val;
 }
 
 void init(Fifo * fifo){
 	fifo->size =-1;
- 	
     fifo->head = fifo->tail = fifo->nelements =0;
-    
+    for(int i=0;i<QUEUE_SIZE;i++){
+        fifo->queue[i]=-1;
+    }
    
 }
 
