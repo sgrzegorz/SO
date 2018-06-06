15b1c7c060e8b5f666b47303d2ea4b367a640882
sgrzegorz <grzegorzsroka2@gmail.com>
2018-06-06 20:34:25 +0200

    hejo

--- cw9/zad1/zad.c
+++ cw9/zad1/zad.c
@@ -10,13 +10,15 @@
 #include <sys/resource.h>
 #define _GNU_SOURCE
 #define FAILURE_EXIT(format, ...) { char buffer[255]; sprintf(buffer, format, ##__VA_ARGS__); write(1, buffer, strlen(buffer));exit(-1);}
+#define MAG  "\x1B[35m"
+#define CYN  "\x1B[36m"
 
 int N;
 char *buffor[4096];
 int L;
 int P, K, N;
 char file_name[40];
-int search_mode,print_mode,nk;
+int search_mode,verbose,nk;
 int  consument_index, producer_index;
 mutex_t *mutex;
 cond_t not_full, not_empty;
@@ -31,34 +33,45 @@ typedef struct{
 
 Buffer buf;
 
-int produce(Buffer *buf,char * string){
+void signalHandler(int signo){
+    printf("I received signal %s\n",signo == SIGINT ? "SIGINT" : "SIGALRM");
+    exit(0);
+}
+
+int produce(Buffer *buf){
     if(buf ->nelements == buf->size) return -1; //no place to push 
-    buf->array[buf->produce_i] = val;
+    if(verbose) printf("I allocate memory\n");
+    char * string = malloc(4096);
+    fgets(string, 4096, file);
+    buf->array[buf->produce_i] = string;
+
     buf -> produce_i = (buf->produce_i +1) % buf->size;
-    
     buf ->nelements++;
     return 0;
 }
 
-int * consume(Buffer * buf){
+int consume(Buffer * buf){
     if(buf ->nelements == 0) return -1; // no element to pop
     char* string =  buf->array[consume_i];
     buf->array[consume_i] = NULL;
-    buf->consume_i = (buf->consume_i +1) % buf->size;
-    buf-> nelements--;
+   
 
     switch(search_mode){
         case -1:
-            if(strlen(string) < 
+            if(strlen(string) < L) printf(MAG,"%i: %s",consume_i,string);
+            break; 
         case 0:
-
+            if(strlen(string) == 0) printf(MAG,"%i: %s",consume_i,string); 
+            break;
         case 1:
-
+            if(strlen(string) > 0) printf(MAG,"%i: %s",consume_i,string); 
+            break;
     }
-
+    free(string);
     
-        
-    return val;
+    buf->consume_i = (buf->consume_i +1) % buf->size;
+    buf-> nelements--;
+    return 0;
 }
 
 
@@ -91,20 +104,20 @@ void * doConsumerWork(void *thread_i){
 
 void printInfo(){
     printf("Please enter correct arguments:\n");
-    printf("./zad <buffer size> <L> <configuration file path>\n");
-    printf("<L>: -1 lower , 0 equal, 1 greater\n");
-    printf("Configuration args: P K N file_name search_mode print_mode nk\n")
-    printf("./zad 45 -1 ../configuration.txt \n");
+    printf("./zad  <configuration file path>\n");
+    printf("Configuration args: P K N file_name search_mode <L> verbose nk\n")
+    printf("<search_mode>: -1 lower , 0 equal, 1 greater than <L>\n");
+    printf("./zad ../configuration.txt \n");
     exit(0);
 }
 
 void parseCommandArgs(int argc, char * argv[]){
-    if(argc !=3) printInfo();
-    arg
-    FILE *file;
-    if((file = fopen(argv[1],"r") == NULL) FAILURE_EXIT("Opening configuration file failed\n");
+    if(argc !=2) printInfo();
+    
+    FILE *configuration;
+    if((configuration = fopen(argv[1],"r") == NULL) FAILURE_EXIT("Opening configuration file failed\n");
     int N; 
-    if(fscanf("%i %i %i %s %i %i %i",&P,&K,&N,file_name,&search_mode,&print_mode,&nk)==-1) FAILURE_EXIT("Error while parsing:%s",argv[2]);
+    if(fscanf("%i %i %i %s %i %i %i %i",&P,&K,&N,file_name,&search_mode,&L,&verbose,&nk)==-1) FAILURE_EXIT("Error while parsing:%s",argv[1]);
     
     if(N >= QUEUE_SIZE) FAILURE_EXIT("Given buffer size can't be larger than %i",MAX_BUFF_SIZE);
     buf->size = N;
@@ -115,11 +128,17 @@ void parseCommandArgs(int argc, char * argv[]){
         array[i] = NULL;
     }
 
-    fclose(file);
+    fclose(configuration);
 }
 
+void releaseResources(){
+    for(int i=0;i<fifo->size;i++){
+        pthread_exit()
+    }
+}
 
 int main(int argc, char * argv[]){
+    if(atexit(releaseResources)!=0) FAILURE_EXIT("Failed to set atexit function\n");
     parseCommandArgs(argc,argv);
     if((file = fopen(file_name,"r")) == NULL) FAILURE_EXIT("Opening: %s failed\n",file_name); 
     pthread_mutex_init()
@@ -135,7 +154,6 @@ int main(int argc, char * argv[]){
         pthread_create(&threads[i],NULL,doConsumerWork,(void *) (intptr_t) i);  
     }
      
-    
 
 
 
