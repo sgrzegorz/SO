3d0a330db80b57ce3ff921a262537b209560a6e5
sgrzegorz <grzegorzsroka2@gmail.com>
2018-06-08 15:31:16 +0200

    eerrrrror

--- cw9/zad1/zad.c
+++ cw9/zad1/zad.c
@@ -11,12 +11,14 @@
 #include <sys/resource.h>
 char buffer[255];
 #define _GNU_SOURCE
+#define WRITE(format, ...) { char buffer[255]; sprintf(buffer, format, ##__VA_ARGS__); write(1, buffer, strlen(buffer));}
 #define FAILURE_EXIT(format, ...) { char buffer[255]; sprintf(buffer, format, ##__VA_ARGS__); write(1, buffer, strlen(buffer));exit(-1);}
 #define MAG  "\x1B[35m"
 #define CYN  "\x1B[36m"
 
 #define MAX_BUFFER_SIZE 4096
 
+volatile int end=0;
 int L;
 int P, K;
 int N;
@@ -48,29 +50,34 @@ void * doProducerWork(void * arg){
     while(1){
        
         pthread_mutex_lock(&mutexes[N]);
-       
+
         while(buf.nelements == N){
            
             pthread_cond_wait(&not_full,&mutexes[N]);
             
         }
         
-        pthread_mutex_unlock(&mutexes[buf.produce_i]);
+        pthread_mutex_lock(&mutexes[buf.produce_i]);
         // ------------------- produce ----------------------- 
         if(verbose)printf(MAG"PRODUCER %ld takes line from file\n",pthread_self());
-        char * line = malloc(4096);
-        if(fgets(line, 4096, file) == NULL){
+        char * line = malloc(256);
+        if(fgets(line, 256, file) == NULL){
+            end=1;
             if(verbose)printf(MAG"PRODUCER %ld found EOF\n",pthread_self());
+            
+            pthread_cond_signal(&not_full);
+            pthread_cond_signal(&not_empty);
             pthread_mutex_unlock(&mutexes[buf.produce_i]);
             pthread_mutex_unlock(&mutexes[N]);
             return NULL;
         } 
         
+        
         buf.array[buf.produce_i] = line;
         int previous = buf.produce_i;
         buf.produce_i = (buf.produce_i +1) % N;
         buf.nelements++;
-         if(verbose)printf(MAG"PRODUCER %ld puts line to buf[%i]\n",pthread_self(),previous);
+        if(verbose)printf(MAG"PRODUCER %ld puts line to buf[%i]\n",pthread_self(),previous);
         // ----------------------------------------------------
         pthread_mutex_unlock(&mutexes[previous]);
 
@@ -83,7 +90,7 @@ void * doConsumerWork(void *arg){
     
     while(1){
         
-        pthread_mutex_lock(&mutexes[buf.consume_i]);
+        pthread_mutex_lock(&mutexes[N+1]);
         
         while(buf.nelements == 0){
             pthread_cond_wait(&not_empty,&mutexes[N+1]);
@@ -99,28 +106,31 @@ void * doConsumerWork(void *arg){
        
         char* line =  buf.array[buf.consume_i];
         buf.array[buf.consume_i] = NULL;
+        printf("%ld\n",strlen(line));
+       
 
-        switch(search_mode){
-            case -1:
-                if(strlen(line) < L) printf(CYN"CONSUMER %ld found: %s",pthread_self(),line);
-                break; 
-            case 0:
-                if(strlen(line) == 0) printf(CYN"CONSUMER %ld found: %s",pthread_self(),line); 
-                break;
-            case 1:
-                if(strlen(line) > 0) printf(CYN"CONSUMER %ld found: %s",pthread_self(),line); 
-                break;
-        }
+        // switch(search_mode){
+        //     case -1:
+        //         if(strlen(line) < L) printf(CYN"CONSUMER %ld found: %s",pthread_self(),line);
+        //         break; 
+        //     case 0:
+        //         if(strlen(line) == 0) printf(CYN"CONSUMER %ld found: %s",pthread_self(),line); 
+        //         break;
+        //     case 1:
+        //         if(strlen(line) > 0) printf(CYN"CONSUMER %ld found: %s",pthread_self(),line); 
+        //         break;
+        // }
         
         free(line);
-        
+         
         int previous = buf.consume_i;
         buf.consume_i = (buf.consume_i +1) % N;
         buf.nelements--;
+        
         if(verbose)printf(CYN"CONSUMER %ld freed buf[%i] \n",pthread_self(),previous);
         // -------------------------------------------
         pthread_mutex_unlock(&mutexes[previous]);
-        
+       // printf("qqqqqqqqqqqqqqqqqqqqqqq\n");
 
         pthread_cond_signal(&not_full);
         pthread_mutex_unlock(&mutexes[N+1]);
@@ -177,8 +187,8 @@ int main(int argc, char * argv[]){
     if((file = fopen(file_name,"r")) == NULL) FAILURE_EXIT("Opening: %s failed\n",file_name); 
     
     
-    mutexes = (pthread_mutex_t*) calloc(N,sizeof(pthread_mutex_t));
-    for(int i=0;i<N;i++){
+    mutexes = (pthread_mutex_t*) calloc(N+2,sizeof(pthread_mutex_t));
+    for(int i=0;i<N+2;i++){
         pthread_mutex_init(mutexes+i,NULL);
     }
 
@@ -204,12 +214,12 @@ int main(int argc, char * argv[]){
         if(pthread_join(producer_threads[p],NULL)!=0) FAILURE_EXIT("Waiting for producer thread failed: %s\n",strerror(errno));
     }
     finish_work=1;
-    if(verbose) printf("All producents finished their work\n");
+    if(verbose) WRITE("All producents finished their work\n");
     pthread_cond_broadcast(&not_empty);
     for(int k=0;k<K;k++){
         if(pthread_join(consumer_threads[k],NULL)!=0) FAILURE_EXIT("Waiting for consumer thread failed: %s\n",strerror(errno));
     }
-    if(verbose) printf("All consuments finished their work\n");
+    if(verbose) WRITE("All consuments finished their work\n");
 
     fclose(file);
 }
